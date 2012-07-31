d37c141a1ebe1b34f3367f809ab22cbbbb212ebc
vinayak <vinayak@vin.(none)>
2012-07-31 23:53:45 +0530

    draft

--- explainme.sh
+++ explainme.sh
@@ -14,19 +14,26 @@ print_brackets(){
 tokenize(){
   for i in ${@} 
   do
+    echo $i
     token_type $i  
   done
 }
 
 get_details_command(){
-  apropos $1 | head -1 | tr -s ' ' | cut -d '-' -f2
+  apropos $1 | grep -e "^$1 " | head -1 | tr -s ' ' | cut -d '-' -f2
+
 
 }
 
 get_details_switch(){
-
   SWITCH=-$1
-  man mkdir | grep -e $SWITCH -A 1 | tail -1
+  INFO=`man ${COMMAND_STACK[$PIPES]} | grep -e "  $SWITCH" -A 1`
+#  if [ -z "$INFO" ]; then
+#    echo "fetch 2nd time"
+#    INFO=`man ${COMMAND_STACK[$PIPES]} | grep -e "  $SWITCH"`
+#    echo $INFO
+#  fi
+  echo $INFO
 }
 
 
@@ -53,11 +60,25 @@ tokenize_switch(){
 
 token_type(){
   case $1 in
+     '|') PIPES=$((PIPES + 1))
+          echo "on previous output"
+      ;;
     -*) tokenize_switch $1
       ;;
-     *) echo `get_details_command $1`
+     *) echo "PIPES" $PIPES "COMMAND_STACK" ${#COMMAND_STACK[@]}
+        if [ ${PIPES} = ${#COMMAND_STACK[@]} ]; then
+          COMMAND_STACK+=( $1 )        
+          #echo "command"
+          echo `get_details_command $1`
+        else
+          echo "on input params : " `print_brackets $1`
+        fi
       ;;
   esac
 }
 
+COMMAND_STACK=()
+PIPES=0
 tokenize $PARAMS
+echo $PIPES
+echo ${COMMAND_STACK[@]}
